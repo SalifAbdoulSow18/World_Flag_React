bb8a14a7f3b966329a1285e8aec88412e2e3b827
SalifAbdoulSow18 <abdoulsalifsow06@gmail.com>
2021-06-02 17:08:31 +0000

    mon premier commit pour le projet World flags en reactjs

---
+++ src/components/Countries.js
@@ -0,0 +1,27 @@
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+import Card from "./Card";
+
+const Countries = () => {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        axios.get('http://restcountries.eu/rest/v2/all?fields=name;population;region;capital;flag')
+            .then((res) => {
+                setData(res.data);
+            });
+        console.log(data);
+    },[]);
+
+
+    return (
+        <div className="countries">
+            <ul className="countries-list">
+                {data.map((country) => (
+                    <Card country={country} key={country.name} />
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default Countries;
---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+const About = () => {
+    return (
+        <div>
+            <Navigation />
+            <Logo />
+            <h1>A propos</h1>
+            <br/>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate eligendi exercitationem facilis fugiat maxime! Architecto culpa dignissimos ea, enim, excepturi hic ipsa minima minus modi neque numquam quaerat ratione sequi. Aliquid amet autem, dicta id in laboriosam numquam odit officia optio perferendis, perspiciatis quas quod recusandae veritatis voluptatem. Eius exercitationem facilis nemo nostrum tenetur! Aspernatur dolor expedita molestias nesciunt soluta!</p>
+            <br/>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquam hic illo sapiente sit voluptas? Accusantium autem blanditiis, cumque doloremque eveniet iste iure labore molestiae nam odit pariatur quia quod repellendus sint soluta unde veniam, voluptatum? Accusantium adipisci alias architecto, cupiditate ducimus eius error est eum ex excepturi facilis fuga fugit impedit inventore iste itaque laborum maiores nemo neque obcaecati odit optio pariatur perspiciatis, quaerat qui quibusdam quis reprehenderit tempore ullam unde veritatis vitae voluptate voluptatibus! Aliquid, perferendis repudiandae. Animi dignissimos, excepturi harum laborum laudantium nulla recusandae reprehenderit. Beatae blanditiis consequuntur deleniti doloribus eaque ipsam magni nemo officia, pariatur quae quos recusandae reprehenderit sapiente sed sint, sunt tempora ullam veniam voluptatem voluptatum! Adipisci eligendi iure quisquam? Aperiam beatae consequuntur dicta distinctio esse ex exercitationem facilis, ipsa ipsum iusto laborum minima modi nihil nisi quaerat ratione veniam veritatis. Deserunt, ratione, suscipit.</p>
+        </div>
+    );
+};
+
+export default About;
